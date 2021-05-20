69f9bbbf95c266c2f7d06d73b6a38c853415fc66
Wamala-Emmanuel <kisaakye.emma@gmail.com>
2021-05-20 12:52:05 +0300

    Adding validation to Groups module

--- src/Components/Groups.js
+++ src/Components/Groups.js
@@ -1,64 +1,92 @@
-import React from "react";
-import { Card, Col, Container, Button } from "react-bootstrap";
-import { Form } from "react-bootstrap";
-import axios from "axios";
+import React from "react"
+import { Card, Col, Container, Button } from "react-bootstrap"
+import { Form } from "react-bootstrap"
+import axios from "axios"
 
 class Groups extends React.Component {
-    state = {
-        name: "",
-    };
+  state = {
+    name: "",
+    errors: {},
+  }
 
-    changeHandler = (event) => {
-        this.setState({ [event.target.name]: event.target.value });
-    };
+  changeHandler = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
 
-    submitHandler = (event) => {
-        event.preventDefault();
-        event.target.className += " was-validated";
-        axios
-            .post("https://avcs-platform.herokuapp.com/groups", this.state)
-            .then(() => this.setState(() => ({ name: "" })))        
-            .catch((error) => console.log(error))
+  findFormErrors = () => {
+    const errors = {}
+    let validateName = () => {
+      if (this.state.name === "") {
+        errors.name = "Please provide a group name"
+      }
     }
+    validateName()
 
-    render() {
-        return (
-            <>
-                <Container>
-                    <Card.Body>
-                        <Form
-                            className="needs-validation"
-                            onSubmit={this.submitHandler}
-                            noValidate
-                        >
-                            <h1>Groups</h1>
-                            <Form.Row>
-                                <Col lg="3">
-                                    <Form.Group controlId="formBasicEmail">
-                                        <Form.Label>Group Name</Form.Label>
-                                        <Form.Control
-                                            type="text"
-                                            value={this.state.name}
-                                            onChange={this.changeHandler}
-                                            name="name"
-                                            required
-                                            placeholder="Group name"
-                                        />
-                                        <div className="invalid-feedback">
-                                            Required!
-                                        </div>
-                                    </Form.Group>
-                                </Col>
-                            </Form.Row>
+    return errors
+  }
 
-                            <Button id="add-button" type="submit">
-                                Submit
-                            </Button>
-                        </Form>
-                    </Card.Body>
-                </Container>
-            </>
-        );
+  submitHandler = (event) => {
+    event.preventDefault()
+
+    if (Object.keys(this.findFormErrors()).length === 0) {
+      event.target.className += " was-validated"
+      let temp = {...this.state}
+      delete temp.errors
+      axios
+        .post("https://avcs-platform.herokuapp.com/groups", temp)
+        .then(() => {
+          this.setState(() => ({ name: "" }))
+          event.target.className = "needs-validation"
+        })
+        .catch((error) => console.log(error))
+    } else {
+      let errors = this.findFormErrors()
+      this.setState((prevState) => {
+        return {
+          ...prevState,
+          errors: errors,
+        }
+      })
     }
+  }
+
+  render() {
+    return (
+      <>
+        <Container>
+          <Card.Body>
+            <Form
+              className="needs-validation"
+              onSubmit={this.submitHandler}
+              noValidate
+            >
+              <h1>Groups</h1>
+              <Form.Row>
+                <Form.Group as={Col} lg="3" controlId="name">
+                  <Form.Label>Group Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={this.state.name}
+                    onChange={this.changeHandler}
+                    isInvalid={this.state.errors.name}
+                    name="name"
+                    required
+                    placeholder="Group name"
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {this.state.errors.name}
+                  </Form.Control.Feedback>
+                </Form.Group>
+              </Form.Row>
+
+              <Button id="add-button" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Card.Body>
+        </Container>
+      </>
+    )
+  }
 }
-export default Groups;
+export default Groups
--- src/Components/MaritalStatus.js
+++ src/Components/MaritalStatus.js
@@ -1,64 +1,93 @@
-import React from "react";
-import { Card, Col, Container, Button } from "react-bootstrap";
-import { Form } from "react-bootstrap";
-import axios from "axios";
+import React from "react"
+import { Card, Col, Container, Button } from "react-bootstrap"
+import { Form } from "react-bootstrap"
+import axios from "axios"
 
 class MaritalStatus extends React.Component {
-    state = {
-        name: "",
-    };
+  state = {
+    name: "",
+    errors: {},
+  }
 
-    changeHandler = (event) => {
-        this.setState({ [event.target.name]: event.target.value });
-    };
+  changeHandler = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
 
-    submitHandler = (event) => {
-        event.preventDefault();
-        event.target.className += " was-validated";
-        axios
-            .post("https://avcs-platform.herokuapp.com/maritalStatus", this.state)
-            .then(() => this.setState(() => ({ name: "" })))
-            .catch((error) => console.log(error));
-    };
-
-    render() {
-        return (
-            <>
-                <Container>
-                    <Card.Body>
-                        <Form
-                            className="needs-validation"
-                            onSubmit={this.submitHandler}
-                            noValidate
-                        >
-                            <h1>Marital Status</h1>
-                            <Form.Row>
-                                <Col lg="3">
-                                    <Form.Group controlId="formBasicEmail">
-                                        <Form.Label>Marital Status</Form.Label>
-                                        <Form.Control
-                                            type="text"
-                                            value={this.state.name}
-                                            onChange={this.changeHandler}
-                                            name="name"
-                                            required
-                                            placeholder="Marital Status"
-                                        />
-                                        <div className="invalid-feedback">
-                                            Required!
-                                        </div>
-                                    </Form.Group>
-                                </Col>
-                            </Form.Row>
-
-                            <Button id="add-button" type="submit">
-                                Submit
-                            </Button>
-                        </Form>
-                    </Card.Body>
-                </Container>
-            </>
-        );
+  findFormErrors = () => {
+    const errors = {}
+    let validateName = () => {
+      if (this.state.name === "") {
+        errors.name = "Please provide a marital status"
+      }
     }
+    validateName()
+
+    return errors
+  }
+
+  submitHandler = (event) => {
+    event.preventDefault()
+
+    if (Object.keys(this.findFormErrors()).length === 0) {
+      event.target.className += " was-validated"
+      let temp = { ...this.state }
+      delete temp.errors
+
+      axios
+        .post("https://avcs-platform.herokuapp.com/maritalStatus", temp)
+        .then(() => {
+          this.setState(() => ({ name: "" }))
+          event.target.className = "needs-validation"
+        })
+        .catch((error) => console.log(error))
+    } else {
+      let errors = this.findFormErrors()
+      this.setState((prevState) => {
+        return {
+          ...prevState,
+          errors: errors,
+        }
+      })
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <Container>
+          <Card.Body>
+            <Form
+              className="needs-validation"
+              onSubmit={this.submitHandler}
+              noValidate
+            >
+              <h1>Marital Status</h1>
+              <Form.Row>
+                <Form.Group as={Col} lg="3" controlId="maritalstatus">
+                  <Form.Label>Marital Status</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={this.state.name}
+                    onChange={this.changeHandler}
+                    isInvalid={this.state.errors.name}
+                    name="name"
+                    required
+                    placeholder="Marital Status"
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {this.state.errors.name}
+                  </Form.Control.Feedback>
+                </Form.Group>
+              </Form.Row>
+
+              <Button id="add-button" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Card.Body>
+        </Container>
+      </>
+    )
+  }
 }
-export default MaritalStatus;
+export default MaritalStatus
