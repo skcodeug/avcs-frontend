87bd64439c742ae137fcc67840fb24fb2e24737d
unknown <BernardM@LUG-PC-001.laboremus.local>
2021-07-20 14:58:08 +0300

    Adding vertical nav

--- src/Components/ConsultantFirmConsultants/ConsultanFirmConsultants.js
+++ src/Components/ConsultantFirmConsultants/ConsultanFirmConsultants.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Card, Col, Container, Button } from "react-bootstrap";
-import { Form } from "react-bootstrap";
+import { Form, Button, Navbar, Nav, Container } from "react-bootstrap";
 import axios from "axios";
 import findFormErrors from "./FindFormErrors";
 import NavBar from "../NavBar";
@@ -102,17 +101,123 @@ class ConsultantFirmConsultants extends React.Component {
 
   render() {
     return (
-      <Container>
-        <Card.Body>
-          <Form
-            className="needs-validation"
-            onSubmit={this.submitHandler}
-            noValidate
+      <>
+        <Navbar
+          collapseOnSelect
+          expand="lg"
+          bg="dark"
+          variant="dark"
+          style={{ position: "fixed", zIndex: "1", right: "0", left: "0" }}
+        >
+          <Navbar.Brand href="/dashboard/users">AVCS</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/dashboard/users">Dashboard</Nav.Link>
+              <Nav.Link href="/reports/users">Reports</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link eventKey={2} href="/logout">
+                Log out
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <div style={{ display: "flex" }}>
+          <Nav
+            defaultActiveKey="/home"
+            style={{
+              height: "100%",
+              width: "19%",
+              marginTop: "5%",
+              paddingBottom: "8%",
+              paddingTop: "1%",
+              overflowX: "hidden",
+              overflowY: "auto",
+              position: "fixed",
+              zIndex: "1",
+              top: "0",
+              left: "0",
+              display: "block",
+            }}
           >
-            <NavBar /> <br />
-            <h1>Consultant Firm Consultants</h1>
-            <Form.Row>
-              <Form.Group as={Col} controlId="consultantId">
+            <Nav.Link href="/dashboard/users">Users</Nav.Link>
+            <Nav.Link href="/dashboard/operations">Operations</Nav.Link>
+            <Nav.Link href="/dashboard/clients">Clients</Nav.Link>
+            <Nav.Link href="/dashboard/maritalstatus">Marital Status</Nav.Link>
+            <Nav.Link href="/dashboard/annualleave">Annual Leave</Nav.Link>
+            <Nav.Link href="/dashboard/annualleavedepartments">
+              Annual Leave Departments
+            </Nav.Link>
+            <Nav.Link href="/dashboard/annualleavemds">
+              Annual Leave Mds
+            </Nav.Link>
+            <Nav.Link href="/dashboard/annualleaverecommenders">
+              Annual Leave Recommenders
+            </Nav.Link>
+            <Nav.Link href="/dashboard/approvals">Approvals</Nav.Link>
+            <Nav.Link href="/dashboard/biodata">Bio data</Nav.Link>
+            <Nav.Link href="/dashboard/clientcategories">
+              Client Categories
+            </Nav.Link>
+            <Nav.Link href="/dashboard/consultantfirmconsultants">
+              Consultant Firm Consultants
+            </Nav.Link>
+            <Nav.Link href="/dashboard/consultantcategories">
+              Consultant Categories
+            </Nav.Link>
+            <Nav.Link href="/dashboard/consultantfirms">
+              Consultant Firms
+            </Nav.Link>
+            <Nav.Link href="/dashboard/consultants">Consultants</Nav.Link>
+            <Nav.Link href="/dashboard/corporateclients">
+              Corporate Clients
+            </Nav.Link>
+            <Nav.Link href="/dashboard/departments">Departments</Nav.Link>
+            <Nav.Link href="/dashboard/individualclients">
+              Individual Clients
+            </Nav.Link>
+            <Nav.Link href="/dashboard/individualconsultants">
+              Individual Consultants
+            </Nav.Link>
+            <Nav.Link href="/dashboard/interviewevaluation">
+              Interview Evaluation
+            </Nav.Link>
+            <Nav.Link href="/dashboard/invoices">Invoices</Nav.Link>
+            <Nav.Link href="/dashboard/nextofkin">Next of Kin</Nav.Link>
+            <Nav.Link href="/dashboard/payments">Payments</Nav.Link>
+            <Nav.Link href="/dashboard/projectstatus">Project Status</Nav.Link>
+            <Nav.Link href="/dashboard/prospects">Prospects</Nav.Link>
+            <Nav.Link href="/dashboard/qualifications">Qualifications</Nav.Link>
+            <Nav.Link href="/dashboard/quotation">Quotation</Nav.Link>
+            <Nav.Link href="/dashboard/quotationdetails">
+              Quotation Details
+            </Nav.Link>
+            <Nav.Link href="/dashboard/receipts">Receipts</Nav.Link>
+            <Nav.Link href="/dashboard/requisitions">Requisitions</Nav.Link>
+            <Nav.Link href="/dashboard/staffverifications">
+              Staff Verifications
+            </Nav.Link>
+            <Nav.Link href="/dashboard/userdetails1">User Details I</Nav.Link>
+            <Nav.Link href="/dashboard/userdetails2">User Details II</Nav.Link>
+            <Nav.Link href="/dashboard/userrelations">User Relations</Nav.Link>
+          </Nav>
+
+          <Container>
+            <Form
+              className="needs-validation"
+              onSubmit={this.submitHandler}
+              noValidate
+              style={{
+                marginLeft: "15%",
+                paddingTop: "2%",
+                marginTop: "8%",
+                marginBottom: "10%",
+              }}
+            >
+              <h1>Consultant Firm Consultants</h1>
+
+              <Form.Group controlId="consultantId">
                 <Form.Label>Consultant ID</Form.Label>
                 <Form.Control
                   type="text"
@@ -128,7 +233,7 @@ class ConsultantFirmConsultants extends React.Component {
                 </Form.Control.Feedback>
               </Form.Group>
 
-              <Form.Group as={Col} controlId="firstname">
+              <Form.Group controlId="firstname">
                 <Form.Label>First Name</Form.Label>
                 <Form.Control
                   type="text"
@@ -137,14 +242,14 @@ class ConsultantFirmConsultants extends React.Component {
                   name="firstName"
                   required
                   isInvalid={this.state.errors.firstName}
-                  placeholder="First Name"
+                  placeholder="e.g John"
                 />
                 <Form.Control.Feedback type="invalid">
                   {this.state.errors.firstName}
                 </Form.Control.Feedback>
               </Form.Group>
 
-              <Form.Group as={Col} controlId="surname">
+              <Form.Group controlId="surname">
                 <Form.Label>Surname</Form.Label>
                 <Form.Control
                   type="text"
@@ -153,14 +258,14 @@ class ConsultantFirmConsultants extends React.Component {
                   name="surname"
                   required
                   isInvalid={this.state.errors.surname}
-                  placeholder="Surname"
+                  placeholder="e.g Doe"
                 />
                 <Form.Control.Feedback type="invalid">
                   {this.state.errors.surname}
                 </Form.Control.Feedback>
               </Form.Group>
 
-              <Form.Group as={Col} controlId="othernames">
+              <Form.Group controlId="othernames">
                 <Form.Label>Other Names</Form.Label>
                 <Form.Control
                   type="text"
@@ -169,15 +274,14 @@ class ConsultantFirmConsultants extends React.Component {
                   name="otherNames"
                   required
                   isInvalid={this.state.errors.otherNames}
-                  placeholder="Other Names"
+                  placeholder="e.g Steve"
                 />
                 <Form.Control.Feedback type="invalid">
                   {this.state.errors.otherNames}
                 </Form.Control.Feedback>
               </Form.Group>
-            </Form.Row>
-            <Form.Row>
-              <Form.Group as={Col} controlId="dob">
+
+              <Form.Group controlId="dob">
                 <Form.Label>Date Of Birth</Form.Label>
                 <Form.Control
                   type="date"
@@ -194,7 +298,7 @@ class ConsultantFirmConsultants extends React.Component {
                 </Form.Control.Feedback>
               </Form.Group>
 
-              <Form.Group as={Col} controlId="gender">
+              <Form.Group controlId="gender">
                 <Form.Label>Gender</Form.Label>
                 <Form.Control
                   as="select"
@@ -217,7 +321,7 @@ class ConsultantFirmConsultants extends React.Component {
                 </Form.Control.Feedback>
               </Form.Group>
 
-              <Form.Group as={Col} controlId="maritalstatus">
+              <Form.Group controlId="maritalstatus">
                 <Form.Label>Marital Status</Form.Label>
                 <Form.Control
                   as="select"
@@ -240,9 +344,8 @@ class ConsultantFirmConsultants extends React.Component {
                   {this.state.errors.maritalStatusId}
                 </Form.Control.Feedback>
               </Form.Group>
-            </Form.Row>
-            <Form.Row>
-              <Form.Group as={Col} controlId="nin">
+
+              <Form.Group controlId="nin">
                 <Form.Label>NIN</Form.Label>
                 <Form.Control
                   type="text"
@@ -251,14 +354,14 @@ class ConsultantFirmConsultants extends React.Component {
                   name="nationalIdNumber"
                   required
                   isInvalid={this.state.errors.nationalIdNumber}
-                  placeholder="National Id Numner"
+                  placeholder="e.g CM12345678UEBC"
                 />
                 <Form.Control.Feedback type="invalid">
                   {this.state.errors.nationalIdNumber}
                 </Form.Control.Feedback>
               </Form.Group>
 
-              <Form.Group as={Col} controlId="email">
+              <Form.Group controlId="email">
                 <Form.Label>Email Address</Form.Label>
                 <Form.Control
                   type="email"
@@ -267,6 +370,7 @@ class ConsultantFirmConsultants extends React.Component {
                   onChange={this.changeHandler}
                   name="email"
                   required
+                  placeholder="e.g abc@avcs.ug"
                   isInvalid={this.state.errors.email}
                 />
                 <Form.Control.Feedback type="invalid">
@@ -274,7 +378,7 @@ class ConsultantFirmConsultants extends React.Component {
                 </Form.Control.Feedback>
               </Form.Group>
 
-              <Form.Group as={Col} controlId="phonenumber">
+              <Form.Group controlId="phonenumber">
                 <Form.Label>Phone number</Form.Label>
                 <Form.Control
                   type="text"
@@ -283,14 +387,14 @@ class ConsultantFirmConsultants extends React.Component {
                   name="phoneNumbers"
                   required
                   isInvalid={this.state.errors.phoneNumber}
-                  placeholder="Enter phone numbers e.g. 0700237434, 0779883527"
+                  placeholder="e.g. 0700237434, 0779883527"
                 />
                 <Form.Control.Feedback type="invalid">
                   {this.state.errors.phoneNumber}
                 </Form.Control.Feedback>
               </Form.Group>
 
-              <Form.Group as={Col} controlId="consultancyRate">
+              <Form.Group controlId="consultancyRate">
                 <Form.Label>Consultancy rate</Form.Label>
                 <Form.Control
                   type="text"
@@ -299,19 +403,20 @@ class ConsultantFirmConsultants extends React.Component {
                   name="consultancyRate"
                   required
                   isInvalid={this.state.errors.consultancyRate}
-                  placeholder="Enter phone numbers e.g. 0700237434, 0779883527"
+                  placeholder="e.g. 0700237434, 0779883527"
                 />
                 <Form.Control.Feedback type="invalid">
                   {this.state.errors.consultancyRate}
                 </Form.Control.Feedback>
               </Form.Group>
-            </Form.Row>
-            <Button id="add-button" type="submit">
-              Submit
-            </Button>
-          </Form>
-        </Card.Body>
-      </Container>
+
+              <Button id="add-button" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Container>
+        </div>
+      </>
     );
   }
 }
