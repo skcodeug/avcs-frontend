1770d7126820ef996db273099580262fc84b0b51
unknown <BernardM@LUG-PC-001.laboremus.local>
2021-07-20 14:06:57 +0300

    Adding vertical nav

--- src/Components/Approvals/Approvals.js
+++ src/Components/Approvals/Approvals.js
@@ -1,76 +1,181 @@
-import React from "react"
-import { Card, Col, Container, Button } from "react-bootstrap"
-import { Form } from "react-bootstrap"
-import axios from "axios"
-import findFormErrors from "./FindFormErrors"
-import NavBar from "../NavBar"
+import React from "react";
+import { Form, Button, Navbar, Nav, Container } from "react-bootstrap";
+import axios from "axios";
+import findFormErrors from "./FindFormErrors";
 
 class Approvals extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
       date: "",
       requisitionReferenceId: "",
       approverId: "",
-      errors: {}
-    }
+      errors: {},
+    };
   }
 
   changeHandler = (event) => {
-    this.setState({ [event.target.name]: event.target.value })
-  }
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
   submitHandler = (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
     if (Object.keys(findFormErrors(this.state)).length === 0) {
-      event.target.className += " was-validated"
+      event.target.className += " was-validated";
 
-      let temp = { ...this.state }
-      delete temp.errors
-      console.log(temp)
+      let temp = { ...this.state };
+      delete temp.errors;
+      console.log(temp);
 
       axios
         .post("https://avcs-platform.herokuapp.com/approvals", temp, {
           headers: {
             Authorization:
-              "Bearer " + localStorage.getItem("access-token").replace(/"/g, "")
-          }
+              "Bearer " +
+              localStorage.getItem("access-token").replace(/"/g, ""),
+          },
         })
         .then(() => {
           this.setState(() => ({
             date: "",
             requisitionReferenceId: "",
             approverId: "",
-            errors: {}
-          }))
-          event.target.className = "needs-validation"
+            errors: {},
+          }));
+          event.target.className = "needs-validation";
         })
-        .catch((error) => console.log(error))
+        .catch((error) => console.log(error));
     } else {
-      let errors = findFormErrors(this.state)
+      let errors = findFormErrors(this.state);
       this.setState((prevState) => {
         return {
           ...prevState,
-          errors: errors
-        }
-      })
+          errors: errors,
+        };
+      });
     }
-  }
+  };
 
   render() {
     return (
-      <Container>
-        <Card.Body>
-          <Form
-            className="needs-validation"
-            onSubmit={this.submitHandler}
-            noValidate
+      <>
+        <Navbar
+          collapseOnSelect
+          expand="lg"
+          bg="dark"
+          variant="dark"
+          style={{ position: "fixed", zIndex: "1", right: "0", left: "0" }}
+        >
+          <Navbar.Brand href="/dashboard/users">AVCS</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/dashboard/users">Dashboard</Nav.Link>
+              <Nav.Link href="/reports/users">Reports</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link eventKey={2} href="/logout">
+                Log out
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <div style={{ display: "flex" }}>
+          <Nav
+            defaultActiveKey="/home"
+            style={{
+              height: "100%",
+              width: "19%",
+              marginTop: "5%",
+              paddingBottom: "8%",
+              paddingTop: "1%",
+              overflowX: "hidden",
+              overflowY: "auto",
+              position: "fixed",
+              zIndex: "1",
+              top: "0",
+              left: "0",
+              display: "block",
+            }}
           >
-            <NavBar /> <br />
-            <h1>Approvals</h1>
-            <Form.Row>
-              <Form.Group as={Col} controlId="date">
+            <Nav.Link href="/dashboard/users">Users</Nav.Link>
+            <Nav.Link href="/dashboard/operations">Operations</Nav.Link>
+            <Nav.Link href="/dashboard/clients">Clients</Nav.Link>
+            <Nav.Link href="/dashboard/maritalstatus">Marital Status</Nav.Link>
+            <Nav.Link href="/dashboard/annualleave">Annual Leave</Nav.Link>
+            <Nav.Link href="/dashboard/annualleavedepartments">
+              Annual Leave Departments
+            </Nav.Link>
+            <Nav.Link href="/dashboard/annualleavemds">
+              Annual Leave Mds
+            </Nav.Link>
+            <Nav.Link href="/dashboard/annualleaverecommenders">
+              Annual Leave Recommenders
+            </Nav.Link>
+            <Nav.Link href="/dashboard/approvals">Approvals</Nav.Link>
+            <Nav.Link href="/dashboard/biodata">Bio data</Nav.Link>
+            <Nav.Link href="/dashboard/clientcategories">
+              Client Categories
+            </Nav.Link>
+            <Nav.Link href="/dashboard/consultantfirmconsultants">
+              Consultant Firm Consultants
+            </Nav.Link>
+            <Nav.Link href="/dashboard/consultantcategories">
+              Consultant Categories
+            </Nav.Link>
+            <Nav.Link href="/dashboard/consultantfirms">
+              Consultant Firms
+            </Nav.Link>
+            <Nav.Link href="/dashboard/consultants">Consultants</Nav.Link>
+            <Nav.Link href="/dashboard/corporateclients">
+              Corporate Clients
+            </Nav.Link>
+            <Nav.Link href="/dashboard/departments">Departments</Nav.Link>
+            <Nav.Link href="/dashboard/individualclients">
+              Individual Clients
+            </Nav.Link>
+            <Nav.Link href="/dashboard/individualconsultants">
+              Individual Consultants
+            </Nav.Link>
+            <Nav.Link href="/dashboard/interviewevaluation">
+              Interview Evaluation
+            </Nav.Link>
+            <Nav.Link href="/dashboard/invoices">Invoices</Nav.Link>
+            <Nav.Link href="/dashboard/nextofkin">Next of Kin</Nav.Link>
+            <Nav.Link href="/dashboard/payments">Payments</Nav.Link>
+            <Nav.Link href="/dashboard/projectstatus">Project Status</Nav.Link>
+            <Nav.Link href="/dashboard/prospects">Prospects</Nav.Link>
+            <Nav.Link href="/dashboard/qualifications">Qualifications</Nav.Link>
+            <Nav.Link href="/dashboard/quotation">Quotation</Nav.Link>
+            <Nav.Link href="/dashboard/quotationdetails">
+              Quotation Details
+            </Nav.Link>
+            <Nav.Link href="/dashboard/receipts">Receipts</Nav.Link>
+            <Nav.Link href="/dashboard/requisitions">Requisitions</Nav.Link>
+            <Nav.Link href="/dashboard/staffverifications">
+              Staff Verifications
+            </Nav.Link>
+            <Nav.Link href="/dashboard/userdetails1">User Details I</Nav.Link>
+            <Nav.Link href="/dashboard/userdetails2">User Details II</Nav.Link>
+            <Nav.Link href="/dashboard/userrelations">User Relations</Nav.Link>
+          </Nav>
+
+          <Container>
+            <Form
+              className="needs-validation"
+              onSubmit={this.submitHandler}
+              noValidate
+              style={{
+                marginLeft: "15%",
+                paddingTop: "2%",
+                marginTop: "8%",
+                marginBottom: "10%",
+              }}
+            >
+              <h1>Approvals</h1>
+
+              <Form.Group controlId="date">
                 <Form.Label>Date</Form.Label>
                 <Form.Control
                   type="date"
@@ -86,7 +191,7 @@ class Approvals extends React.Component {
                 </Form.Control.Feedback>
               </Form.Group>
 
-              <Form.Group as={Col} controlId="requisitionReferenceId">
+              <Form.Group controlId="requisitionReferenceId">
                 <Form.Label>Requisition Reference ID</Form.Label>
                 <Form.Control
                   type="text"
@@ -102,7 +207,7 @@ class Approvals extends React.Component {
                 </Form.Control.Feedback>
               </Form.Group>
 
-              <Form.Group as={Col} controlId="approverId">
+              <Form.Group controlId="approverId">
                 <Form.Label>Approver ID</Form.Label>
                 <Form.Control
                   type="text"
@@ -117,14 +222,15 @@ class Approvals extends React.Component {
                   {this.state.errors.approverId}
                 </Form.Control.Feedback>
               </Form.Group>
-            </Form.Row>
-            <Button id="add-button" type="submit">
-              Submit
-            </Button>
-          </Form>
-        </Card.Body>
-      </Container>
-    )
+
+              <Button id="add-button" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Container>
+        </div>
+      </>
+    );
   }
 }
-export default Approvals
+export default Approvals;
--- src/Components/BioData/BioData.js
+++ src/Components/BioData/BioData.js
@@ -1,9 +1,7 @@
 import React from "react";
-import { Card, Col, Container, Button } from "react-bootstrap";
-import { Form } from "react-bootstrap";
+import { Form, Button, Navbar, Nav, Container } from "react-bootstrap";
 import axios from "axios";
 import findFormErrors from "./FindFormErrors";
-import NavBar from "../NavBar";
 
 class BioData extends React.Component {
   constructor() {
@@ -71,17 +69,123 @@ class BioData extends React.Component {
 
   render() {
     return (
-      <Container>
-        <Card.Body>
-          <Form
-            className="needs-validation"
-            onSubmit={this.submitHandler}
-            noValidate
+      <>
+        <Navbar
+          collapseOnSelect
+          expand="lg"
+          bg="dark"
+          variant="dark"
+          style={{ position: "fixed", zIndex: "1", right: "0", left: "0" }}
+        >
+          <Navbar.Brand href="/dashboard/users">AVCS</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/dashboard/users">Dashboard</Nav.Link>
+              <Nav.Link href="/reports/users">Reports</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link eventKey={2} href="/logout">
+                Log out
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <div style={{ display: "flex" }}>
+          <Nav
+            defaultActiveKey="/home"
+            style={{
+              height: "100%",
+              width: "19%",
+              marginTop: "5%",
+              paddingBottom: "8%",
+              paddingTop: "1%",
+              overflowX: "hidden",
+              overflowY: "auto",
+              position: "fixed",
+              zIndex: "1",
+              top: "0",
+              left: "0",
+              display: "block",
+            }}
           >
-            <NavBar /> <br />
-            <h1>BioData</h1>
-            <Form.Row>
-              <Form.Group as={Col} controlId="staffId">
+            <Nav.Link href="/dashboard/users">Users</Nav.Link>
+            <Nav.Link href="/dashboard/operations">Operations</Nav.Link>
+            <Nav.Link href="/dashboard/clients">Clients</Nav.Link>
+            <Nav.Link href="/dashboard/maritalstatus">Marital Status</Nav.Link>
+            <Nav.Link href="/dashboard/annualleave">Annual Leave</Nav.Link>
+            <Nav.Link href="/dashboard/annualleavedepartments">
+              Annual Leave Departments
+            </Nav.Link>
+            <Nav.Link href="/dashboard/annualleavemds">
+              Annual Leave Mds
+            </Nav.Link>
+            <Nav.Link href="/dashboard/annualleaverecommenders">
+              Annual Leave Recommenders
+            </Nav.Link>
+            <Nav.Link href="/dashboard/approvals">Approvals</Nav.Link>
+            <Nav.Link href="/dashboard/biodata">Bio data</Nav.Link>
+            <Nav.Link href="/dashboard/clientcategories">
+              Client Categories
+            </Nav.Link>
+            <Nav.Link href="/dashboard/consultantfirmconsultants">
+              Consultant Firm Consultants
+            </Nav.Link>
+            <Nav.Link href="/dashboard/consultantcategories">
+              Consultant Categories
+            </Nav.Link>
+            <Nav.Link href="/dashboard/consultantfirms">
+              Consultant Firms
+            </Nav.Link>
+            <Nav.Link href="/dashboard/consultants">Consultants</Nav.Link>
+            <Nav.Link href="/dashboard/corporateclients">
+              Corporate Clients
+            </Nav.Link>
+            <Nav.Link href="/dashboard/departments">Departments</Nav.Link>
+            <Nav.Link href="/dashboard/individualclients">
+              Individual Clients
+            </Nav.Link>
+            <Nav.Link href="/dashboard/individualconsultants">
+              Individual Consultants
+            </Nav.Link>
+            <Nav.Link href="/dashboard/interviewevaluation">
+              Interview Evaluation
+            </Nav.Link>
+            <Nav.Link href="/dashboard/invoices">Invoices</Nav.Link>
+            <Nav.Link href="/dashboard/nextofkin">Next of Kin</Nav.Link>
+            <Nav.Link href="/dashboard/payments">Payments</Nav.Link>
+            <Nav.Link href="/dashboard/projectstatus">Project Status</Nav.Link>
+            <Nav.Link href="/dashboard/prospects">Prospects</Nav.Link>
+            <Nav.Link href="/dashboard/qualifications">Qualifications</Nav.Link>
+            <Nav.Link href="/dashboard/quotation">Quotation</Nav.Link>
+            <Nav.Link href="/dashboard/quotationdetails">
+              Quotation Details
+            </Nav.Link>
+            <Nav.Link href="/dashboard/receipts">Receipts</Nav.Link>
+            <Nav.Link href="/dashboard/requisitions">Requisitions</Nav.Link>
+            <Nav.Link href="/dashboard/staffverifications">
+              Staff Verifications
+            </Nav.Link>
+            <Nav.Link href="/dashboard/userdetails1">User Details I</Nav.Link>
+            <Nav.Link href="/dashboard/userdetails2">User Details II</Nav.Link>
+            <Nav.Link href="/dashboard/userrelations">User Relations</Nav.Link>
+          </Nav>
+
+          <Container>
+            <Form
+              className="needs-validation"
+              onSubmit={this.submitHandler}
+              noValidate
+              style={{
+                marginLeft: "15%",
+                paddingTop: "2%",
+                marginTop: "8%",
+                marginBottom: "10%",
+              }}
+            >
+              <h1>BioData</h1>
+
+              <Form.Group controlId="staffId">
                 <Form.Label>Staff ID</Form.Label>
                 <Form.Control
                   type="text"
@@ -97,7 +201,7 @@ class BioData extends React.Component {
                 </Form.Control.Feedback>
               </Form.Group>
 
-              <Form.Group as={Col} controlId="residence">
+              <Form.Group controlId="residence">
                 <Form.Label>Residence</Form.Label>
                 <Form.Control
                   type="text"
@@ -106,14 +210,14 @@ class BioData extends React.Component {
                   name="residence"
                   required
                   isInvalid={this.state.errors.residence}
-                  placeholder="Enter residence"
+                  placeholder="e.g Muyenga"
                 />
                 <Form.Control.Feedback type="invalid">
                   {this.state.errors.residence}
                 </Form.Control.Feedback>
               </Form.Group>
 
-              <Form.Group as={Col} controlId="nextOfKinId">
+              <Form.Group controlId="nextOfKinId">
                 <Form.Label>Next of kin ID</Form.Label>
                 <Form.Control
                   type="text"
@@ -128,9 +232,8 @@ class BioData extends React.Component {
                   {this.state.errors.nextOfKinId}
                 </Form.Control.Feedback>
               </Form.Group>
-            </Form.Row>
-            <Form.Row>
-              <Form.Group as={Col} controlId="employmentExperiences">
+
+              <Form.Group controlId="employmentExperiences">
                 <Form.Label>Employment experiences</Form.Label>
                 <Form.Control
                   type="text"
@@ -139,14 +242,14 @@ class BioData extends React.Component {
                   name="employmentExperiences"
                   required
                   isInvalid={this.state.errors.employmentExperiences}
-                  placeholder="Enter text here"
+                  placeholder="e.g I was a leader ..."
                 />
                 <Form.Control.Feedback type="invalid">
                   {this.state.errors.employmentExperiences}
                 </Form.Control.Feedback>
               </Form.Group>
 
-              <Form.Group as={Col} controlId="responsibilitiesBefore">
+              <Form.Group controlId="responsibilitiesBefore">
                 <Form.Label>Responsibilities before</Form.Label>
                 <Form.Control
                   type="date"
@@ -163,14 +266,14 @@ class BioData extends React.Component {
                 </Form.Control.Feedback>
               </Form.Group>
 
-              <Form.Group as={Col} controlId="personalSkills">
+              <Form.Group controlId="personalSkills">
                 <Form.Label>Personal skills</Form.Label>
                 <Form.Control
                   type="text"
                   value={this.state.personalSkills}
                   onChange={this.changeHandler}
                   name="personalSkills"
-                  placeholder="Enter personal skills"
+                  placeholder="e.g Communication"
                   required
                   isInvalid={this.state.errors.personalSkills}
                 />
@@ -178,9 +281,8 @@ class BioData extends React.Component {
                   {this.state.errors.personalSkills}
                 </Form.Control.Feedback>
               </Form.Group>
-            </Form.Row>
-            <Form.Row>
-              <Form.Group as={Col} controlId="periodOfAvailability">
+
+              <Form.Group controlId="periodOfAvailability">
                 <Form.Label>Period of availability</Form.Label>
                 <Form.Control
                   type="text"
@@ -188,13 +290,14 @@ class BioData extends React.Component {
                   name="periodOfAvailability"
                   required
                   isInvalid={this.state.errors.periodOfAvailability}
+                  placeholder="e.g 6 months"
                 />
                 <Form.Control.Feedback type="invalid">
                   {this.state.errors.periodOfAvailability}
                 </Form.Control.Feedback>
               </Form.Group>
 
-              <Form.Group as={Col} controlId="expectedDate">
+              <Form.Group controlId="expectedDate">
                 <Form.Label>Expected date</Form.Label>
                 <Form.Control
                   type="text"
@@ -203,19 +306,20 @@ class BioData extends React.Component {
                   name="expectedDate"
                   required
                   isInvalid={this.state.errors.expectedDate}
-                  placeholder="National Id Numner"
+                  placeholder="e.g CM12347634KDFJ"
                 />
                 <Form.Control.Feedback type="invalid">
                   {this.state.errors.expectedDate}
                 </Form.Control.Feedback>
               </Form.Group>
-            </Form.Row>
-            <Button id="add-button" type="submit">
-              Submit
-            </Button>
-          </Form>
-        </Card.Body>
-      </Container>
+
+              <Button id="add-button" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Container>
+        </div>
+      </>
     );
   }
 }
